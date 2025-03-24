4230465dc0cd623e085e81ad7e6705f1b1e03705
Karsten Lehmann <mail@kalehmann.de>
2025-03-24 20:57:04 +0100

    feat: Change frequency with arrow keys

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,54 @@ use rustfft::{num_complex::Complex, Fft, FftPlanner};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
+use sdl2::rect::Rect;
+use sdl2::rwops::RWops;
 use std::ops::DerefMut;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+const ANDIKA_BOLD_TTF: &[u8] = include_bytes!("../assets/Andika/Andika-Bold.ttf");
 const WIDTH: u32 = 1024;
-const HEIGHT: u32 = 800;
+const HEIGHT: u32 = 700;
 const CHANNELS: u32 = 3;
 const BUF_SIZE: usize = (WIDTH * HEIGHT * CHANNELS) as usize;
 const FFT_SIZE: usize = WIDTH as usize;
 const PITCH: u32 = WIDTH * CHANNELS;
 
 struct WaterfallDemo {
+    center_frequency: Arc<AtomicU32>,
     control_thread: Option<thread::JoinHandle<()>>,
     should_stop: Arc<AtomicBool>,
     video_buffer: Arc<Mutex<Vec<u8>>>,
+}
+
+fn render_text_centered<S, T>(
+    text: &str,
+    x: i32,
+    y: i32,
+    font: &sdl2::ttf::Font,
+    canvas: &mut sdl2::render::Canvas<S>,
+    texture_creator: &sdl2::render::TextureCreator<T>,
+) where
+    S: sdl2::render::RenderTarget,
+{
+    let surface = font
+        .render(text)
+        .blended(Color::RGBA(255, 255, 255, 255))
+        .unwrap();
+    let texture = texture_creator
+        .create_texture_from_surface(&surface)
+        .unwrap();
+    let sdl2::render::TextureQuery { width, height, .. } = texture.query();
+    let rect = Rect::new(
+        x - (width / 2) as i32,
+        y - (height / 2) as i32,
+        width,
+        height,
+    );
+    canvas.copy(&texture, None, rect).unwrap();
 }
 
 fn shift<T>(buf: &mut [T], shape: Vec<u32>, axis: usize, d: i32)
@@ -92,6 +123,7 @@ fn work_fft(buf: &[u8], fft: Arc<dyn Fft<f64>>, video_buffer: Arc<Mutex<Vec<u8>>
 impl WaterfallDemo {
     pub fn new() -> WaterfallDemo {
         WaterfallDemo {
+            center_frequency: Arc::new(AtomicU32::new(100_000_000)),
             control_thread: None,
             should_stop: Arc::new(AtomicBool::new(false)),
             video_buffer: Arc::new(Mutex::new(vec![0u8; BUF_SIZE])),
@@ -108,13 +140,15 @@ impl WaterfallDemo {
     }
 
     pub fn start_control_thread(&mut self) {
+        let center_frequency = self.center_frequency.clone();
         let should_stop = self.should_stop.clone();
         let video_buffer = self.video_buffer.clone();
         self.control_thread = Some(thread::spawn(move || {
             let (mut ctl, mut reader) =
                 rtlsdr_mt::open(0).expect("Could not open RTL-SDR device at index 0.");
             ctl.set_sample_rate(2_400_000).unwrap();
-            ctl.set_center_freq(100_000_000).unwrap();
+            ctl.set_center_freq(center_frequency.load(Ordering::Relaxed))
+                .unwrap();
 
             let reader_thread = thread::spawn(move || {
                 let mut planner: FftPlanner<f64> = FftPlanner::new();
@@ -128,6 +162,10 @@ impl WaterfallDemo {
             });
 
             while !should_stop.load(Ordering::Relaxed) {
+                let cf = center_frequency.load(Ordering::Relaxed);
+                if cf != ctl.center_freq() {
+                    ctl.set_center_freq(cf);
+                }
                 thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
             }
             ctl.cancel_async_read();
@@ -138,9 +176,14 @@ impl WaterfallDemo {
     pub fn start_sdl2_window(&self) {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
+        let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
+        let mut font = ttf_context
+            .load_font_from_rwops(RWops::from_bytes(ANDIKA_BOLD_TTF).unwrap(), 32)
+            .unwrap();
+        font.set_style(sdl2::ttf::FontStyle::BOLD);
 
         let window = video_subsystem
-            .window("Rust RTL-SDR waterfall demo", WIDTH, HEIGHT)
+            .window("Rust RTL-SDR waterfall demo", WIDTH, HEIGHT + 100)
             .position_centered()
             .build()
             .unwrap();
@@ -165,6 +208,22 @@ impl WaterfallDemo {
                         self.should_stop.store(true, Ordering::Relaxed);
                         break 'running;
                     }
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Left),
+                        ..
+                    } => {
+                        self.center_frequency.store(
+                            self.center_frequency.load(Ordering::Relaxed) - 100_000,
+                            Ordering::Relaxed,
+                        );
+                    }
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Right),
+                        ..
+                    } => self.center_frequency.store(
+                        self.center_frequency.load(Ordering::Relaxed) + 100_000,
+                        Ordering::Relaxed,
+                    ),
                     _ => {}
                 }
             }
@@ -182,8 +241,18 @@ impl WaterfallDemo {
                 let texture = texture_creator
                     .create_texture_from_surface(&surface)
                     .unwrap();
-                canvas.copy(&texture, None, None).unwrap();
+                let r = Rect::new(0, 0, WIDTH, HEIGHT);
+                canvas.copy(&texture, r, r).unwrap();
             }
+            let freq_mhz = (self.center_frequency.load(Ordering::Relaxed) as f64) / 1_000_000f64;
+            render_text_centered(
+                &format!("{freq_mhz:.3} MHz").to_string(),
+                (WIDTH / 2) as i32,
+                750,
+                &font,
+                &mut canvas,
+                &texture_creator,
+            );
 
             canvas.present();
             thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
